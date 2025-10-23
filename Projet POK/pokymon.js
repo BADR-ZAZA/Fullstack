2df2cbc741feb31d5e8d2200953363c3de2412fd
BADR ZAZA <badryoucan7@gmail.com>
2025-10-23 10:15:55 +0000

    Delete Projet POK directory

--- Projet POK/pokymon.js
+++
@@ -1,8 +0,0 @@
-const qEl = document.getItemById('#q');
-const statusEl = document.getItemById('#status');
-const cardEl = document.getItemById('#card');
-const historyEl = document.getItemById('#history')
-
-fetch('https://pokeapi.co/api/v2/', {
-    
-})
